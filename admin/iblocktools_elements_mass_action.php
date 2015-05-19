c893cf81231e5536f100618f7a79dea8f4dd4392
Kudin Sasha <kudinsasha@gmail.com>
2015-05-19 10:37:18 +0300

    массовое изменение полей элементов

--- admin/iblocktools_elements_mass_action.php
+++ admin/iblocktools_elements_mass_action.php
@@ -8,39 +8,52 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_admin_a
 $fields = array('NAME', 'CODE', 'EXTERNAL_ID', 'SORT', 'PREVIEW_TEXT', 'DETAIL_TEXT', 'TAGS');
 
 if ($_REQUEST['submit']) {
-    
-    $arSelect = Array("ID", "NAME", "IBLOCK_ID");
+    $fieldName = $_REQUEST['field'];
+    // никакой валидации! всё через жопу! будьте аккуратны
+    $arSelect = Array("ID", "IBLOCK_ID", $fieldName);
     $arFilter = Array("IBLOCK_ID"=>$_REQUEST['IBLOCK_ID']);
-    $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>5000), $arSelect);
-    echo '<pre>';
-    while($arFields = $res->GetNext()) {
-          
-  /*      $newField = htmlspecialchars_decode($arFields['NAME']);
-         
-        $el = new CIBlockElement;
-        $result = $el->Update($arFields['ID'], array('NAME' => $newField));
-         
+    $res = CIBlockElement::GetList(Array('timestamp_x' => 'DESC'), $arFilter, false, Array("nPageSize"=>5000), $arSelect);
+    echo '<pre>'; 
+    $el = new CIBlockElement;
+    while($arFields = $res->GetNext()) { 
+        $field = $arFields[$fieldName];
+        echo "<b>" . $arFields['ID'] . "</b>\n" . $field, "\n";  
+        eval($_REQUEST['code']); // АМИНЬ !  
+        echo $field, "\n"; 
+        $result = $el->Update($arFields['ID'], array($fieldName => $field));
         if($result) {
-            echo 'Изменён элемент ' . $arFields['ID'] . "\n";
+            echo 'Изменён успешно';
         } else {
-            echo $el->LAST_ERROR . "\n";
-        }
-        */
+            echo $el->LAST_ERROR;
+        } 
+        echo "\n\n";
     }
     echo '</pre>';
     
 } else {
     ?> 
-    <form method="POST" method="POST">
-        <? IblocktoolsFormsHelper::showIblockListSelect(); ?>
-        <br><br>
-        <select name='field'><?
-        foreach($fields as $field) { ?><option name="<?=$field;?>"><?=$field;?></option><? } ?></select>
-        <br><br>
-        <textarea name='code' style="width: 500px; height: 130px; resize: none;">$field = str_replace(' ', '_', $field);</textarea>
-        <br><br>
-        <input type="submit" name="submit" value="Предпросмотр"> 
-    </form> 
+        <table>
+        <tr>
+            <td width='50%' valign='top'> 
+                <form method="POST" method="POST">
+                    <? IblocktoolsFormsHelper::showIblockListSelect(); ?>
+                    <br><br>
+                    <select name='field'><? foreach ($fields as $field) { ?><option name="<?= $field; ?>"><?= $field; ?></option><? } ?></select>
+                    <br><br>
+                    Значение поля попадает в переменную $field<br>
+                    <textarea name='code' style="width: 500px; height: 130px; resize: none;">$field = str_replace(' ', '_', $field);</textarea>
+                    <br><br>
+                    <input type="submit" name="submit" value="Предпросмотр"> 
+                </form>  
+            </td>
+            <td width='50%' valign='top'>
+                <pre>$field = htmlspecialchars_decode($field);
+
+$field = strtoupper($field);
+                </pre>
+            </td>
+        </tr>
+    </table> 
     <?
 }
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_admin.php");
